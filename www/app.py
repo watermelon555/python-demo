489638aa0d34108baa6860a55ac53e63dae4bf2e
xinan.zeng <xinan.zeng@tengwo.com.cn>
2020-12-24 19:05:51 +0800

    服务器连接，数据库初始化

---
+++ www/app.py
@@ -0,0 +1,21 @@
+import logging ;logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time 
+from datetime import datetime 
+from aiohttp import web 
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+# ,content_type='text/html'  不添加会变成下载文件非直接浏览
+
+# @asyncio.coroutine# @asyncio.coroutine 在版本3已经改成async def的格式，更加简便
+async def init(loop):
+    app = web.Application(loop = loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
